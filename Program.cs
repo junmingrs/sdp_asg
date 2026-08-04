d64cf3b78b29b92d3f46eaf3d30bc1ddd4f9c95a
Key <Taylonteo@gmail.com>
2026-08-04 09:58:34 +0800

    Testing if git works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SDP_ASG;
+Console.WriteLine("Testing commit 123");
+Test testing = new Test("testing123");
+
+testing.testtest();
